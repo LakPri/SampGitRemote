6c4b86f3c6e87f7672b6ac6454d0cd4154940e24
PRIYA <priya@wpdm0108.be.fortis.bank>
2018-03-05 14:09:02 +0100

    First commit

---
+++ SampleGitApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleGitApp
+//
+//  Created by PRIYA on 05/03/2018.
+//  Copyright © 2018 PRIYA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SampleGitApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SampleGitApp
+//
+//  Created by PRIYA on 05/03/2018.
+//  Copyright © 2018 PRIYA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
