8a0bf317630c7a33b14d8ef74076cace31c26317
PRIYA <priya@WPDM0108.local>
2018-03-22 12:21:52 +0100

    Commitrebase

--- SampleGitApp/ViewController.m
+++ SampleGitApp/ViewController.m
@@ -16,6 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSLog(@"Helloffgfgfgf");NSLog(@"Helloffgfgfgf");NSLog(@"Helloffgfgfgf");NSLog(@"Helloffgfgfgf");NSLog(@"Helloffgfgfgf");NSLog(@"Helloffgfgfgf");NSLog(@"Helloffgfgfgf");NSLog(@"Helloffgfgfgf");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
